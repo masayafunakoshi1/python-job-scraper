2f09b49c0741432283e342cf9ce289fa86aff841
Masaya Funakoshi <masayafunakoshi1@gmail.com>
2023-02-02 22:33:12 -0500

    Cleaned up LinkedIn script

--- scripts/linkedin_web_scraper.py
+++ scripts/linkedin_web_scraper.py
@@ -3,10 +3,6 @@ from bs4 import BeautifulSoup
 from datetime import date
 from sheets_append_values import append_values
 
-#insert console inputted URL logic here
-# URL = 'https://www.linkedin.com/jobs/view/3451031538/?alternateChannel=search&refId=Pn3zSJd09WKgEGmIwhjSrg%3D%3D&trackingId=dadK7Hxc%2FQ65gAzJMnnnyA%3D%3D'
-# URL = 'https://www.linkedin.com/jobs/view/3457349697/?alternateChannel=search&refId=Pn3zSJd09WKgEGmIwhjSrg%3D%3D&trackingId=X4avWcEW%2F88OV1XMvw8Xkg%3D%3D&trk=d_flagship3_job_details'
-
 #stores info that will go into google docs
 docs_info = {
     "date": date.today().strftime("%m/%d/%y"),
@@ -37,8 +33,10 @@ def title_info_splitter(title, url):
 def scraping_handler(url):
     pageinfo = requests.get(url)
 
-    soup = BeautifulSoup(pageinfo.text, 'html.parser')
+    soup = BeautifulSoup(pageinfo.text, "html.parser")
     html_title = soup.title.string
+    # html_with_info = soup.find("body").find("").find(class_="jobs-unified-top-card ")
+    # print(html_with_info)
 
     title_info_splitter(html_title, url)
     
