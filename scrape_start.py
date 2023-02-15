9bfa88b95c7e35ec55cfc1c2e7822cdad6983dc5
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2023-02-14 21:46:17 -0500

    added curr dir location w/ os

--- scrape_start.py
+++ scrape_start.py
@@ -1,7 +1,10 @@
 import sys
 import re
+import subprocess
+import os
 
-sys.path.insert(0, "/mnt/d/coding/programming_projects/Extra-projects/python-job-scraper/scripts")
+CURR_DIR = os.getcwd()
+sys.path.insert(0, f"{CURR_DIR}/scripts")
 import linkedin_web_scraper
 import greenhouse_web_scraper
 import workday_web_scraper
@@ -9,6 +12,11 @@ import workday_web_scraper
 print("Input job URL: ")
 URL = str(input())
 
+#quit out of program
+if(URL == "quit" or URL == "q"):
+    print("Great job! You got this!")
+    sys.exit()
+    
 # search for LinkedIn, Greenhouse, or Workday in URL, determine which script to run
 if re.search("linkedin", URL):
     linkedin_web_scraper.scraping_handler(URL)
@@ -16,4 +24,9 @@ elif re.search("greenhouse", URL):
     greenhouse_web_scraper.scraping_handler(URL)
 elif re.search("workday", URL):
     workday_web_scraper.scraping_handler(URL)
+else:
+    print("Invalid URL, please try again.")
 
+print("\n")
+
+subprocess.run(["python3 scrape_start.py"], shell=True)
